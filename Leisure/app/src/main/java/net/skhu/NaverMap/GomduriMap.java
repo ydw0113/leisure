be12cdc8b6d29aa44eb6e2200c89abfff8e7667f
ydw0113 <ydw212901@gmail.com>
2018-11-15 17:38:07 +0900

    Create GomduriMap.java

---
+++ Leisure/app/src/main/java/net/skhu/NaverMap/GomduriMap.java
@@ -0,0 +1,167 @@
+package net.skhu.NaverMap;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+
+import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapView;
+import com.nhn.android.maps.maplib.NGeoPoint;
+import com.nhn.android.maps.nmapmodel.NMapError;
+import com.nhn.android.maps.overlay.NMapPOIdata;
+import com.nhn.android.maps.overlay.NMapPOIitem;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
+import com.nhn.android.mapviewer.overlay.NMapResourceProvider;
+
+import net.skhu.leisure.R;
+
+public class GomduriMap extends NMapActivity {
+
+    private final String  TAG = "GomduriMap";
+
+    private ViewGroup mapLayout;
+
+    private NMapController mMapController;
+    private NMapView mMapView;
+
+    private NMapResourceProvider nMapResourceProvider;
+    private NMapOverlayManager mapOverlayManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_gomdurimap);
+
+        init();
+
+        nMapResourceProvider = new NMapViewerResourceProvider(this);
+        mapOverlayManager = new NMapOverlayManager(this, mMapView, nMapResourceProvider);
+    }
+
+    private void init(){
+
+
+        mapLayout = findViewById(R.id.mapLayout);
+
+        mMapView = new NMapView(this);
+        mMapView.setClientId(getResources().getString(R.string.NAVER_API_KEY)); // 클라이언트 아이디 값 설정
+        mMapView.setClickable(true);
+        mMapView.setEnabled(true);
+        mMapView.setFocusable(true);
+        mMapView.setFocusableInTouchMode(true);
+        mMapView.setScalingFactor(1.7f);        //이것을 안해주면 줌시켜서 축소 시켜도 작게보
+        mMapView.requestFocus();
+
+        mMapView.setOnMapStateChangeListener(changeListener);
+        mMapView.setOnMapViewTouchEventListener(mapListener);
+        mapLayout.addView(mMapView);
+
+        mMapController = mMapView.getMapController();
+        mMapController.setMapCenter(new NGeoPoint(127.135454,37.500494), 11);     //Default Data
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setMarker();
+            }
+        }, 2000);
+    }
+
+    private void setMarker(){
+
+        int markerId = NMapPOIflagType.PIN;
+
+        // set POI data
+        NMapPOIdata poiData = new NMapPOIdata(1, nMapResourceProvider);
+        poiData.beginPOIdata(1);
+        poiData.addPOIitem(127.135454,37.500494, "서울곰두리체육관", markerId, 0);
+        poiData.endPOIdata();
+
+        // create POI data overlay
+        NMapPOIdataOverlay poiDataOverlay = mapOverlayManager.createPOIdataOverlay(poiData, null);
+        poiDataOverlay.showAllPOIdata(13);
+        poiDataOverlay.setOnStateChangeListener(onPOIdataStateChangeListener);  //좌표 클릭시 말풍선 리스
+    }
+
+    private NMapPOIdataOverlay.OnStateChangeListener onPOIdataStateChangeListener = new NMapPOIdataOverlay.OnStateChangeListener() {
+        @Override
+        public void onFocusChanged(NMapPOIdataOverlay nMapPOIdataOverlay, NMapPOIitem nMapPOIitem) {
+
+        }
+
+        @Override
+        public void onCalloutClick(NMapPOIdataOverlay nMapPOIdataOverlay, NMapPOIitem nMapPOIitem) {
+            if (nMapPOIitem != null) {
+                Log.e(TAG, "onFocusChanged: " + nMapPOIitem.toString());
+            } else {
+                Log.e(TAG, "onFocusChanged: ");
+            }
+        }
+    };
+
+
+    private NMapView.OnMapStateChangeListener changeListener = new NMapView.OnMapStateChangeListener() {
+        @Override
+        public void onMapInitHandler(NMapView nMapView, NMapError nMapError) {
+            Log.e(TAG, "OnMapStateChangeListener onMapInitHandler : ");
+        }
+
+        @Override
+        public void onMapCenterChange(NMapView nMapView, NGeoPoint nGeoPoint) {
+            Log.e(TAG, "OnMapStateChangeListener onMapCenterChange : " + nGeoPoint.getLatitude() + " ㅡ  " + nGeoPoint.getLongitude());
+        }
+
+        @Override
+        public void onMapCenterChangeFine(NMapView nMapView) {
+            Log.e(TAG, "OnMapStateChangeListener onMapCenterChangeFine : ");
+        }
+
+        @Override
+        public void onZoomLevelChange(NMapView nMapView, int i) {
+            Log.e(TAG, "OnMapStateChangeListener onZoomLevelChange : " + i);
+        }
+
+        @Override
+        public void onAnimationStateChange(NMapView nMapView, int i, int i1) {
+            Log.e(TAG, "OnMapStateChangeListener onAnimationStateChange : ");
+        }
+    };
+
+    private NMapView.OnMapViewTouchEventListener mapListener = new NMapView.OnMapViewTouchEventListener() {
+        @Override
+        public void onLongPress(NMapView nMapView, MotionEvent motionEvent) {
+            Log.e(TAG, "OnMapViewTouchEventListener onLongPress : ");
+        }
+
+        @Override
+        public void onLongPressCanceled(NMapView nMapView) {
+            Log.e(TAG, "OnMapViewTouchEventListener onLongPressCanceled : ");
+        }
+
+        @Override
+        public void onTouchDown(NMapView nMapView, MotionEvent motionEvent) {
+            Log.e(TAG, "OnMapViewTouchEventListener onTouchDown : ");
+        }
+
+        @Override
+        public void onTouchUp(NMapView nMapView, MotionEvent motionEvent) {
+            Log.e(TAG, "OnMapViewTouchEventListener onTouchUp : ");
+        }
+
+        @Override
+        public void onScroll(NMapView nMapView, MotionEvent motionEvent, MotionEvent motionEvent1) {
+            Log.e(TAG, "OnMapViewTouchEventListener onScroll : ");
+        }
+
+        @Override
+        public void onSingleTapUp(NMapView nMapView, MotionEvent motionEvent) {
+            Log.e(TAG, "OnMapViewTouchEventListener onSingleTapUp : ");
+        }
+    };
+}
